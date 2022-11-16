050e68d098ef44a74d096d31bd3be2396b3e0605
Ken <geubaelee@gmail.com>
2022-11-16 12:57:13 +0900

    feat: Styled Components 을 이용한 Navbar 스타일링

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React, { ReactNode } from 'react'
+import styled from 'styled-components'
 
 export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
 	return (
@@ -11,9 +12,32 @@ export const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
 	)
 }
 
+export const NavbarWrapper = styled.div`
+	width: 100%;
+	height: 40px;
+	display: flex;
+	padding-top: 16px;
+	align-content: center;
+	ul {
+		list-style: none;
+		margin: 0 auto;
+		padding: 0;
+		li {
+			float: left;
+			display: inline-block;
+			margin: 0 8px;
+			padding: 0;
+
+			&:last-child {
+				float: none;
+			}
+		}
+	}
+`
+
 export const Navbar: React.FC<{ children?: ReactNode }> = ({ children }) => {
 	return (
-		<>
+		<NavbarWrapper>
 			<ul>
 				<li>
 					<Link href="/">Home</Link>
@@ -28,7 +52,7 @@ export const Navbar: React.FC<{ children?: ReactNode }> = ({ children }) => {
 					<Link href="/mypage/profile">Profile</Link>
 				</li>
 			</ul>
-		</>
+		</NavbarWrapper>
 	)
 }
 
