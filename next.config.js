2e4e0d707035a75a573dd0a9a920efd440431e92
Ken <geubaelee@gmail.com>
2022-11-16 13:52:13 +0900

    feat: next img 를 위한 host 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		minimumCacheTTL: process.env.NODE_ENV === 'production' ? 60 * 60 * 24 * 30 : 0,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'unsplash.it',
+				// port: '',
+				// pathname: '/**',
+			},
+			// {
+			// 	protocol: 'https',
+			// 	hostname: 'example.com',
+			// 	port: '',
+			// 	pathname: '/account123/**',
+			// },
+		],
+	},
 }
 
 module.exports = nextConfig
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import Image from 'next/image'
+
+import vercelLogoSvg from '../../public/vercel.svg'
 
 const AboutPage: React.FC<{}> = () => {
-	return <div>About</div>
+	const imgUrl = 'https://cdn.solvook.com/static/assets/images02.jpg'
+	return (
+		<div style={{ padding: 30 }}>
+			<div>
+				<img src={'/vercel.svg'} />
+			</div>
+			<div>
+				<Image src={'/vercel.svg'} alt={'vercel-logo'} width={283} height={64} />
+			</div>
+			<div>
+				<Image
+					src={vercelLogoSvg}
+					alt={'vercel-logo'}
+					// width={283} height={64}
+				/>
+			</div>
+			<div>
+				<img src={'https://unsplash.it/640/425'} />
+			</div>
+			<div>
+				<Image
+					src={'https://unsplash.it/640/425'}
+					alt={'cover-img'}
+					width={640}
+					height={425}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default AboutPage
