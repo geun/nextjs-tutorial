84b606a77d6d7ad1ebc3e8f35124524028baed61
Ken <geubaelee@gmail.com>
2022-11-16 12:49:55 +0900

    feat: Styled Components 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import GlobalStyle from '../components/globalstyles'
 import { Layout } from '../components/layout'
+
+const theme: DefaultTheme = {
+	colors: {
+		primary: '#111',
+		secondary: '#0070f3',
+	},
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</>
 	)
 }
