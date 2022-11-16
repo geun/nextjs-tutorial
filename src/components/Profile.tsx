a35995bae95a803b39ab66accb2669aa7e4036d9
Ken <geubaelee@gmail.com>
2022-11-16 11:37:04 +0900

    feat: swr을 이용한 데이터 호출

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,21 +1,30 @@
+import useSWR from 'swr'
 import { useEffect, useState } from 'react'
 
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
 export const Profile: React.FC<{}> = () => {
-	const [data, setData] = useState<{ name: string; bio: string } | null>(null)
-	const [isLoading, setLoading] = useState(false)
+	// const [data, setData] = useState<{ name: string; bio: string } | null>(null)
+	// const [isLoading, setLoading] = useState(false)
 
-	useEffect(() => {
-		setLoading(true)
-		fetch('/api/profile-data')
-			.then((res) => res.json())
-			.then((data) => {
-				setData(data)
-				setLoading(false)
-			})
-	}, [])
+	const { data, error } = useSWR('/api/profile-data', fetcher)
+	//
+	// useEffect(() => {
+	// 	setLoading(true)
+	// 	fetch('/api/profile-data')
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			setData(data)
+	// 			setLoading(false)
+	// 		})
+	// }, [])
 
-	if (isLoading) return <p>Loading...</p>
-	if (!data) return <p>No profile data</p>
+	if (error) return <div>Failed to load</div>
+	if (!data) return <div>Loading...</div>
+
+	// if (isLoading) return <p>Loading...</p>
+	// if (!data) return <p>No profile data</p>
 
 	return (
 		<div>
